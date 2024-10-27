51a5cf358c33f4fc68fc60f103fd1e801cbea63a
AlexandreAdam <adam.alexandre01123@gmail.com>
2024-10-27 10:47:56 -0400

    Refactored the Hutchinson's trick function

--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -105,3 +105,32 @@ def test_solver_step(steps, time_steps):
     assert torch.all(torch.isfinite(samples))
     assert torch.allclose(samples.mean(dim=0), mean, atol=1), "mean for MVG samples not close"
     assert torch.allclose(samples.std(dim=0), cov.sqrt(), atol=1), "std for MVG samples not close"
+
+
+@pytest.mark.skip("Currently not passing, need to debug")
+@pytest.mark.parametrize(
+    "steps,time_steps",
+    (
+        (50, None),  # 50 steps normally
+        (None, torch.linspace(1, 0, 50)),  # 50 steps set by user
+        (None, torch.cat((torch.logspace(0, -2, 49), torch.zeros(1))),),  # 50 steps with log spacing
+    ),
+)
+def test_solver_logprob(steps, time_steps):
+    sde = VESDE(sigma_min=1e-2, sigma_max=100)
+    mean = torch.zeros(2, dtype=torch.float32)
+    # diagonal covariance matrix
+    cov = torch.eye(2, dtype=torch.float32) * 0.1
+    model = MVGScoreModel(
+        sde,
+        mean=mean,
+        cov=cov,
+    )
+
+    x = torch.rand(100, 2, dtype=torch.float32)
+    logp = model.log_prob(x, steps=steps, time_steps=time_steps)
+    true_logp = torch.distributions.MultivariateNormal(mean, cov).log_prob(x)
+    
+    print(logp - true_logp)
+    assert torch.all(torch.isfinite(logp))
+    assert torch.allclose(logp, true_logp, atol=1e-3), "logp for MVG samples not close"
